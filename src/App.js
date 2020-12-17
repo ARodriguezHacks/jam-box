2b480eaa72bab2cc5f1e55f395b2a1613a400e23
Angie Rodriguez <angrodri902@gmail.com>
2020-12-17 14:53:06 -0800

    Add basic CSS grid

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { useContext } from "react";
 import UserContext from "./contexts/UserContext";
 import Header from "./components/Layout/Header";
-import Form from "./components/Form";
+import Content from "./components/Layout/Content";
+import Footer from "./components/Layout/Footer";
+//import Form from "./components/Form";
 
 function App() {
-  const { currentUser } = useContext(UserContext);
+  //const { currentUser } = useContext(UserContext);
 
-  console.log(currentUser);
+  //console.log(currentUser);
   return (
-    <main>
+    <div className="container">
       <Header />
-      <h1>Jam Box</h1>
-      <h2>Welcome {currentUser}!</h2>
-      <Form />
-    </main>
+      <Content />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer>
+      <h3>Designed and Developed by Angie Rodriguez</h3>
+      <nav>
+        <ul>
+          <li>Github</li>
+          <li>Website</li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
