cf17df16dc1889e6009bbaa2d264e823fe414ac2
Angie Rodriguez <angrodri902@gmail.com>
2021-02-05 02:05:41 -0800

    Add sample music data and configure mongo and starter routes

---
+++ backend/data/music.js
@@ -0,0 +1,53 @@
+const songs = [
+  {
+    id: 1,
+    title: "Apex",
+    artist: "Alexander Nakarada",
+    track: "/music/apex.mp3",
+    duration: "5.17",
+  },
+  {
+    id: 2,
+    title: "Bass Meant Jazz",
+    artist: "Kevin MacLeod",
+    track: "/music/bass-meant-jazz.mp3",
+    duration: "4.40",
+  },
+  {
+    id: 3,
+    title: "Finally See The Light",
+    artist: "Bryan Teoh",
+    track: "/music/finally-see-the-light.mp3",
+    duration: "3.21",
+  },
+  {
+    id: 4,
+    title: "Jack The Lumberer",
+    artist: "Alexander Nakarada",
+    track: "/music/jack-the-lumberer.mp3",
+    duration: "2.50",
+  },
+  {
+    id: 5,
+    title: "Planeteer Reaction",
+    artist: "Bryan Teoh",
+    track: "/music/planeteer-reaction.mp3",
+    duration: "4.08",
+  },
+  {
+    id: 6,
+    title: "River Meditation",
+    artist: "Jason Shaw",
+    track: "/music/river-meditation.mp3",
+    duration: "2.47",
+  },
+  {
+    id: 7,
+    title: "Sunday Dub",
+    artist: "Kevin MacLeod",
+    track: "/music/sunday-dub.mp3",
+    duration: "3.29",
+  },
+];
+
+export { songs };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import { Switch, Route } from "react-router-dom";
 import Header from "./components/Layout/Header";
 import Content from "./components/Layout/Content";
 import Footer from "./components/Layout/Footer";
-import About from "./components/About";
+import About from "./pages/About";
+import Songs from "./pages/Songs";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main className="content">
         <Switch>
           <Route exact path="/" component={Content} />
+          <Route exact path="/search" component={Songs} />
           <Route exact path="/about" component={About} />
         </Switch>
       </main>
