636b2d29057e6ff315f6eaaf8e214673f3979a8f
Angie Rodriguez <angrodri902@gmail.com>
2021-02-05 02:05:41 -0800

    Add sample music data and configure mongo and starter routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDatabase from "./config/db.js";
+import { songs } from "./data/music.js";
 
+dotenv.config();
 // Initialize express
 const app = express();
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
 
 // Routes
 app.get("/", (req, res) => {
   res.send("Api is running");
+});
+
+app.get("/api/songs", (req, res) => {
+  res.send(songs);
 });
 
 app.get("/about", (req, res) => {
